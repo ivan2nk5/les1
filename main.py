4b04a062b0a0fd7c51194087b4187403aaa36ea2
C2113 <ivanpro8410@gmail.com>
2024-10-26 16:04:56 +0300

    change 2

--- main.py
+++ main.py
@@ -9,9 +9,5 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi(print_hi1)
-
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
