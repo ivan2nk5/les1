51a7dcddf17cab5584119a21463739daab227053
C2113 <ivanpro8410@gmail.com>
2024-11-02 15:34:01 +0200

    edit 2

--- py_les1/Lesson 2.py
+++ py_les1/Lesson 2.py
@@ -1,4 +1,4 @@
-
+import random
 
 class human:
     def __init__(self, name="human"):
@@ -9,3 +9,24 @@ class auto:
     def __init__(self,brand):
         self.brand = brand
         self.passengers  = []
+    def add_passenger(self,*args):
+        for p in args:
+            self.passengers.append(p)
+
+    def  print_p_name(self):
+        if self.passengers:
+            print(f"n of {self.brand} passengers")
+        for passenger  in self.passengers:
+            print(passenger.name)
+        else:
+            print("t  a n o passengers")
+
+a = input("1")
+a = human(name=a)
+kate = human(name="kate")
+nick = human(name="nick")
+
+car = auto("mers")
+car.add_passenger(a , kate , nick)
+
+car.print_p_name()
