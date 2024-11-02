091fae4ecb51205d8759b884156a502dbef761e3
C2113 <ivanpro8410@gmail.com>
2024-11-02 15:16:45 +0200

    edit 1

---
+++ py_les1/Lesson 2.py
@@ -0,0 +1,11 @@
+
+
+class human:
+    def __init__(self, name="human"):
+        self.name = name
+
+class auto:
+
+    def __init__(self,brand):
+        self.brand = brand
+        self.passengers  = []
